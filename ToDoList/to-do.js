f2af327fb066a707d6fd91c3c0756f30d898543c
Roopam Maurya <theannealoisia@gmail.com>
2024-12-11 12:15:52 +0530

    Added time feature and changed storage of tasks

--- ToDoList/to-do.js
+++ ToDoList/to-do.js
@@ -1,4 +1,6 @@
-let taskList = [];
+let taskList =JSON.parse(localStorage.getItem('taskList'))||[];
+
+displayTasks(taskList);
 
 const addButtonElement = document.querySelector('.js-add-btn');
 addButtonElement.addEventListener('click',()=>{
@@ -7,44 +9,26 @@ addButtonElement.addEventListener('click',()=>{
     function addToDoList() {
         let taskName = document.querySelector('.list');
         let taskDate = document.querySelector('.dateSelector');
+        let taskTime = document.querySelector('.timeSelector');
         const taskDeleteButton = 'Delete';
-        taskList.push([taskName.value, taskDate.value, taskDeleteButton]);
-        //console.log(taskList);
+        taskList.push([taskName.value, taskDate.value, taskTime.value, taskDeleteButton]);
+        [taskName.value, taskDate.value, taskTime.value] = ['','',''];
+
         displayTasks(taskList);
-        taskName.value = '';
-        taskDate.value = '';
+        saveToStorage();
     }
-
-/*     //display task list on screen
-    function displayTasks(taskList) {
-        document.querySelector('.result').innerHTML = '';
-        for (let i = 0; i < taskList.length; i++) {
-            for (let j = 0; j < taskList[i].length; j++) {
-                if (j === 0) {
-                    document.querySelector('.result').innerHTML+= `<p class="result-todo">${taskList[i][j]}</p>`;
-                }
-                else if (j === 1) {
-                    document.querySelector('.result').innerHTML += `<p class="result-date">${taskList[i][j]}</p>`;
-                }
-                else {
-                    document.querySelector('.result').innerHTML += `<button class="del-btn" onclick="deleteTask(${i});">${taskList[i][j]}</button>`;
-
-                }
-            }
-        }
-    }
-    */
 
     function displayTasks(taskList){
-        let showResult = document.querySelector('.result');       //displaying task on screen in simpler way.
-        showResult.innerHTML =  '';
-        taskList.forEach((task,index) => {   // replaced function(task,index) to arrow function (task,index) =>
+        let showResult = document.querySelector('.result');
+        showResult.innerHTML='';
+        taskList.forEach((task,index) => {
             showResult.innerHTML += `<p class="result-todo">${task[0]}</p>`;
             showResult.innerHTML += `<p class="result-date">${task[1]}</p>`;
-            showResult.innerHTML += `<button class="del-btn js-del-btn">${task[2]}</button>`;
+            showResult.innerHTML +=`<p class="result-time">${task[2]}</p>`;
+            showResult.innerHTML += `<button class="del-btn js-del-btn">${task[3]}</button>`;
         });
         document.querySelectorAll('.js-del-btn').forEach((btn, index) => {
-            btn.addEventListener('click', () => {// initially we were passing index but it considered index of previous loop causing issue
+            btn.addEventListener('click', () => {
                 deleteTask(index);
             });
         })
@@ -54,5 +38,10 @@ addButtonElement.addEventListener('click',()=>{
     function deleteTask(i) {
         taskList.splice(i,1);
         displayTasks(taskList);
+        saveToStorage();
+    }
+
+    function saveToStorage(){
+        localStorage.setItem('taskList',JSON.stringify(taskList));
     }
 
