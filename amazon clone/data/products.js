febd0fc5bd40072d4788aa4548d2811e448f951b
Roopam Maurya <theannealoisia@gmail.com>
2024-12-16 00:41:52 +0530

    Added content to amazon clone

---
+++ amazon clone/data/products.js
@@ -0,0 +1,45 @@
+let item0 = {
+    image:'book',
+    description:'Before the Coffee Gets Cold',
+    rating:{
+    stars: 4.3,
+    count: '22,317'},
+    discount: '-32%',
+    cost: 374 ,
+    originalPrice: 550.00 ,
+    deliveryDate: 'Get it by Monday,December 16',
+};
+let item1 = {
+    image:'chandelier',
+    description:'Kinis 10Watt Curvy Style 3 Color Led Wall Light for Living Room',
+    rating:{
+    stars: 3.6,
+    count: 86},
+    discount: '-53%',
+    cost: 1399 ,
+    originalPrice: 2999.00 ,
+    deliveryDate: 'Get it by Wednesday,December 18',
+};
+let item2 = {
+    image: 'top',
+    description:'KOTTY Womens Satin Sleeveless Solid Top',
+    rating:{
+    stars: 3.9,
+    count: 120},
+    discount: '-77%',
+    cost: 299 ,
+    originalPrice: 1299.00 ,
+    deliveryDate: 'Get it by Wednesday,December 18',
+};
+let item3={
+    image: 'baking-bowls',
+    description:'KOMUEE Baking Dish Set',
+    rating:{
+    stars: 4.5,
+    count: 897},
+    discount: '-30%',
+    cost: 11918 ,
+    originalPrice: 1299.00 ,
+    deliveryDate: 'Get it by Wednesday,December 18',
+}
+const products=[item0,item1,item2,item3];
--- amazon clone/scripts/amazon-basic.js
+++ amazon clone/scripts/amazon-basic.js
@@ -1,3 +1,4 @@
+// Created selection array for search bar
 let searchArea = ['All','All Categories','Alexa Skills',
     'Amazon Devices', 'Amazon Fashion', 'Amazon Pharmacy', 'Appliances', 'Apps & Games',
     'Audible Audiobooks', 'Baby', 'Beauty', 'Books', 'Car & Motorbike', 'Clothing & Accessories',
@@ -10,75 +11,55 @@ let searchArea = ['All','All Categories','Alexa Skills',
     'Under 500rs', 'Video Games', 'Watches'
 ];
 
+// Created Selector of search bar in JS
 const searchAreaSelector = document.querySelector('.select-search-area-options');
 for(let i=0; i< searchArea.length ; i++){
     searchAreaSelector.innerHTML += `<option>${searchArea[i]}</option>`
 }
 
 
-let item0 = {
-    image:'book',
-    description:'Before the Coffee Gets Cold',
-    rating:{
-    stars: 4.3,
-    count: '22,317'},
-    discount: '-32%',
-    cost: 374 ,
-    originalPrice: 550.00 ,
-    deliveryDate: 'Get it by Monday,December 16',
-};
-let item1 = {
-    image:'chandelier',
-    description:'Kinis 10Watt Curvy Style 3 Color Led Wall Light for Living Room, Led Wall Lamps for Bedroom, Led Wall Scone for Home Decoration, Led Wall Lamp for Home Wall, Led Wall Lamp for Home/Resturant/Cafe/Bar',
-    rating:{
-    stars: 3.6,
-    count: 86},
-    discount: '-53%',
-    cost: 1399 ,
-    originalPrice: 2999.00 ,
-    deliveryDate: 'Get it by Wednesday,December 18',
-};
-let item2 = {
-    image: 'top',
-    description:'KOTTY Womens Satin Sleeveless Solid Top',
-    rating:{
-    stars: 3.9,
-    count: 120},
-    discount: '-77%',
-    cost: 299 ,
-    originalPrice: 1299.00 ,
-    deliveryDate: 'Get it by Wednesday,December 18',
-};
-let item3={
-    image: 'baking-bowls',
-    description:'KOMUEE Baking Dish Set',
-    rating:{
-    stars: 4.5,
-    count: 897},
-    discount: '-30%',
-    cost: 11918 ,
-    originalPrice: 17029 ,
-    deliveryDate: 'Get it by Wednesday,December 18',
-}
-const products=[item0,item1,item2,item3];
-
+// Code used to generate HTML for products
 const renderProduct = document.querySelector('.js-products');
-products.forEach((value,index)=>{
+products.forEach((value)=>{
     renderProduct.innerHTML += `<div class="product-items">
         <img src="images/${value.image}.jpg" class="image-size">
-        <p class="description">${value.description}</p>
-        <p>${value.rating.count}</p>
-        <p><span class="discount">${value.discount}</span>$${value.cost}</p>
-        <p class="org-price">M.R.P.:$${value.originalPrice}</p>
-        <p>FREE Delivery over &#8377;499. Fulfilled by Amazon.</p>
-        <select class="js-quantity-options quantity-options"></select>
-        <button class="add-to-cart">Add to Cart</button><br>
+        <div class="description-section">
+            <div><p class="description">${value.description}</p></div>
+            <div><p class="rating-count">${value.rating.count}</p></div>
+            <div><p class="cost"><span class="discount">${value.discount}</span><sup>&#8377;</sup>${value.cost}<sup>00</sup><span  class="org-price">M.R.P.: <strike>&#8377;${value.originalPrice}</strike></span></p></div>
+            <div class="description-common-section">
+                <p class="free-delivery">FREE Delivery over &#8377;499. Fulfilled by Amazon.</p>
+                <button class="js-addCart add-to-cart">Add to Cart</button>
+                <select class="js-quantity-options quantity-options"></select>
+                <p class="cart-updated"></p>
+            </div>
+        </div>
+        <br>
     </div>`;
 })
 
-const selectQuantityOption = document.querySelector('.js-quantity-options');
-selectQuantityOption.addEventListener('click',()=>{
+
+// Code for Quantity selector
+const selectQuantityOption = document.querySelectorAll('.js-quantity-options');
+selectQuantityOption.forEach((value)=>{
     for(let i=0;i<10;i++){
-        selectQuantityOption.innerHTML += `<option>${i+1}</option>`;
+        value.innerHTML += `<option>${i+1}</option>`;
     }
-});
+})
+
+//Code for Add to Cart updates
+let  cartQuantity = 0;
+const addToCart = document.querySelectorAll('.js-addCart');
+addToCart.forEach((value)=>{
+    value.addEventListener('click',()=>{
+        if(cartQuantity <= 15){
+            cartQuantity++;
+        }
+        document.querySelector('.cart-count').innerHTML = cartQuantity;
+        /*document.querySelector('.cart-updated').innerHTML = 'Added!'; 
+        setTimeout(()=>{
+            document.querySelector('.cart-updated').innerHTML='';
+        },1000);*/
+    });
+})
+
