b136025970f5eac0cf2e12668ac1bd7f2dc11fdc
Roopam Maurya <theannealoisia@gmail.com>
2025-01-07 12:55:41 +0530

    Added dark-theme and styled bird section

--- Plant Information Page/index.js
+++ Plant Information Page/index.js
@@ -3,4 +3,17 @@ import './scripts/succulent.js';
 import './scripts/birds.js';
 import './scripts/aquaticAnimals.js';
 import './scripts/landAnimals.js';
-console.log('hello');
+
+
+//Dark theme
+document.querySelector('.js-dark').addEventListener('click',()=>{
+    document.body.classList.add('dark-theme');
+    document.querySelector('.js-dark').classList.add('visibility');
+    document.querySelector('.js-light').classList.remove('visibility');
+})
+
+document.querySelector('.js-light').addEventListener('click',()=>{
+    document.body.classList.remove('dark-theme');
+    document.querySelector('.js-dark').classList.remove('visibility');
+    document.querySelector('.js-light').classList.add('visibility');
+})
